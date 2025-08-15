441a96bcf1e2b46b169d80175e0292e88d6dc2eb
Liuba <liubabarusch@gmail.com>
2025-08-14 22:20:41 -0500

    change favicon and remove blinking cursor

--- home/home.js
+++ home/home.js
@@ -1,3 +1,13 @@
+// Remove cursor
+document.addEventListener("DOMContentLoaded", function () {
+  const typingEl = document.querySelector(".typing");
+  typingEl.addEventListener("animationend", (e) => {
+    if (e.animationName === "typing") {
+      typingEl.classList.add("done");
+    }
+  });
+});
+
 // Footer
 const footer = document.querySelector(".footer-content");
 const copyright = document.createElement("p");
